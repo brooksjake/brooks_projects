0b1a96e05cb74d6d669a6653cd726755a95b33a1
brooksjake <brookscollegejake@gmail.com>
2023-02-01 12:03:32 -0700

    finished to do list (without local storage)

--- basicprojects/todo-list/main.js
+++ basicprojects/todo-list/main.js
@@ -14,6 +14,52 @@ window.addEventListener('load', () => {
         }
 
         const item_el = document.createElement("div");
-        item_el.classList.add("item")
-    })
-})
+        item_el.classList.add("item");
+
+        const item_content_el = document.createElement("div");
+        item_content_el.classList.add("content");
+
+        item_el.appendChild(item_content_el);
+
+        const item_input_el = document.createElement("input");
+        item_input_el.classList.add("text");
+        item_input_el.type = "text";
+        item_input_el.value = item;
+        item_input_el.setAttribute("readonly", "readonly");
+
+        item_content_el.appendChild(item_input_el);
+
+        const item_actions_el = document.createElement("div");
+        item_actions_el.classList.add("actions");
+
+        const item_edit_el = document.createElement("button");
+        item_edit_el.classList.add("edit")
+        item_edit_el.innerHTML = "Edit";
+
+        const item_delete_el = document.createElement("button");
+        item_delete_el.classList.add("delete");
+        item_delete_el.innerHTML = "Delete";
+
+        item_actions_el.appendChild(item_edit_el);
+        item_actions_el.appendChild(item_delete_el);
+        item_el.appendChild(item_actions_el);
+        list_el.appendChild(item_el);
+
+        input.value = "";
+
+        item_edit_el.addEventListener('click', () => {
+            if (item_edit_el.innerText.toLowerCase() == "edit") {
+                item_input_el.removeAttribute("readonly");
+                item_input_el.focus();
+                item_edit_el.innerText = "Save";
+            } else {
+                item_input_el.setAttribute("readonly", "readonly");
+                item_edit_el.innerText = "Edit"
+            }
+        });
+
+        item_delete_el.addEventListener('click', () => {
+            list_el.removeChild(item_el);
+        });
+    });
+});
